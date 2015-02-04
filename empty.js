223a30c789310ad56ae3610f2db9318aa18dfbfb
test-admin <github.test.adm@gmail.com>
2015-02-04 17:41:01 +0900

    test changes

--- empty.js
+++ empty.js
@@ -19,48 +19,50 @@ $(function () {
         console.log(repos);
         for (var i = 0; i < repos.length; i++) {
         	console.log(repos[i]);
-        	var repo_class;
-        	var header_class;
-        	var h3_class;
-        	switch (repos[i].language){
-			  case 'C#':
-			    repo_class = dotnet_repo_class;
-			    header_class = dotnet_header_class;
-			    h3_class = dotnet_h3_class;
-			    break;
-			  case 'Objective-C':
-			    repo_class = ios_repo_class;
-			    header_class = ios_header_class;
-			    h3_class = ios_h3_class;
-			    break;
-			  case 'Java':
-			    repo_class = android_repo_class;
-			    header_class = android_header_class;
-			    h3_class = android_h3_class;
-			    break;
-			  case 'JavaScript':
-			    repo_class = js_repo_class;
-			    header_class = js_header_class;
-			    h3_class = js_h3_class;
-			    break;
-			}
-	        var repo_container_div = $('<div/>').addClass(repo_class);
-	        var repo_div = $('<div/>').addClass('repo');
-	        var repo_header_div = $('<div/>').addClass(header_class);
-	        var repo_title_h2 = $('<h2/>').addClass('repo-title');
-	        var repo_link_a = $('<a/>').attr("href", repos[i].html_url).text(repos[i].name);
-	        var repo_language_h3 = $('<h3/>').addClass(h3_class);
-	        var repo_description = $('<p/>').addClass('repo-description').text(repos[i].description);
+        	if(repos[i].language == 'C#' || repos[i].language == 'Objective-C' || repos[i].language == 'Java' || repos[i].language == 'JavaScript') {
+        		var repo_class;
+	        	var header_class;
+	        	var h3_class;
+	        	switch (repos[i].language){
+				  case 'C#':
+				    repo_class = dotnet_repo_class;
+				    header_class = dotnet_header_class;
+				    h3_class = dotnet_h3_class;
+				    break;
+				  case 'Objective-C':
+				    repo_class = ios_repo_class;
+				    header_class = ios_header_class;
+				    h3_class = ios_h3_class;
+				    break;
+				  case 'Java':
+				    repo_class = android_repo_class;
+				    header_class = android_header_class;
+				    h3_class = android_h3_class;
+				    break;
+				  case 'JavaScript':
+				    repo_class = js_repo_class;
+				    header_class = js_header_class;
+				    h3_class = js_h3_class;
+				    break;
+				}
+		        var repo_container_div = $('<div/>').addClass(repo_class);
+		        var repo_div = $('<div/>').addClass('repo');
+		        var repo_header_div = $('<div/>').addClass(header_class);
+		        var repo_title_h2 = $('<h2/>').addClass('repo-title');
+		        var repo_link_a = $('<a/>').attr("href", repos[i].html_url).text(repos[i].name);
+		        var repo_language_h3 = $('<h3/>').addClass(h3_class);
+		        var repo_description = $('<p/>').addClass('repo-description').text(repos[i].description);
 
-	        repo_title_h2.append(repo_link_a);
-	        repo_header_div.append(repo_title_h2);
-	        repo_header_div.append(repo_language_h3);
-	        repo_div.append(repo_header_div);
-	        repo_div.append(repo_description);
-	        repo_container_div.append(repo_div);
-	        
-	        $('div#content.isotope').append(repo_container_div);
-	        console.log($('div#content.isotope'));
+		        repo_title_h2.append(repo_link_a);
+		        repo_header_div.append(repo_title_h2);
+		        repo_header_div.append(repo_language_h3);
+		        repo_div.append(repo_header_div);
+		        repo_div.append(repo_description);
+		        repo_container_div.append(repo_div);
+		        
+		        $('div#content.isotope').append(repo_container_div);
+		        console.log($('div#content.isotope'));
+        	}
         };
     });
 
